bdb7752bbb4100fcc4df5e7a10d3435b8e533d3f
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-03-21 22:37:04 +0200

    add axios

--- src/routes/fairytales/index.js
+++ src/routes/fairytales/index.js
@@ -4,12 +4,29 @@ import fairytales from './tales-8f587-export.json';
 import DataTable from '../../components/data-table';
 
 import Header from './../../components/header/'
-
+import axios from 'axios'
 
 class Fairytales extends Component {
-    state = {};
+    constructor (props) {
+        super(props);
+        this.state = {
+
+            data: []
+        }
+
+
+    }
 
     componentDidMount() {
+
+        // axios.get('https://tales-server.herokuapp.com/api/v1/fairytales')
+        //     .then(response => {
+        //
+        //         this.setState({
+        //              data: response.data['fairytales']
+        //         })
+        //     })
+
         const data = fairytales
             .map(({id, name,}) => {
                 const res = {
@@ -26,9 +43,10 @@ class Fairytales extends Component {
     render() {
         const {data} = this.state;
 
-        if (!data) {
+        if (!this.state.data.length) {
             return 'Loading...';
         }
+
 
         return (
             <div>
