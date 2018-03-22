adf8b7efd31ebd905672ba4cac4a32e6b4aab68a
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-03-22 12:52:57 +0200

    add logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './styles.styl';
+import logo from './logo.png'
 
 const Header = ({title, subTitle,background}) => {
 
     return (
         <header className="header" style={{background:`${background}`}}>
+            <a href="/">
+                <img
+                    className="Brand"
+                    src={logo}
+                    alt="logo"/>
+            </a>
         <h1 >{title}</h1>
         <h3>{subTitle}</h3>
         </header>
