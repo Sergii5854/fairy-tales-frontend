737c6e11c90283594a989a687f8f34e7a189b308
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-03-22 11:30:21 +0200

    get tales from server

--- src/routes/fairytales/index.js
+++ src/routes/fairytales/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import fairytales from './tales-8f587-export.json';
+// import fairytales from './tales-8f587-export.json';
 import DataTable from '../../components/data-table';
 
 import Header from './../../components/header/'
@@ -19,34 +19,34 @@ class Fairytales extends Component {
 
     componentDidMount() {
 
-        // axios.get('https://tales-server.herokuapp.com/api/v1/fairytales')
-        //     .then(response => {
-        //
-        //         this.setState({
-        //              data: response.data['fairytales']
-        //         })
-        //     })
+        axios.get('https://tales-server.herokuapp.com/api/v1/fairytales')
+            .then(response => {
+                let data = response.data['fairytales']
+                console.log(data);
+                let dataALL = data
+                    .map(({id, name,}) => {
+                        const res = {
+                            '#': Number(id),
+                            'Name': name
+                        };
 
-        const data = fairytales
-            .map(({id, name,}) => {
-                const res = {
-                    '#': Number(id),
-                    'Name': name
+                        return res;
+                    });
+                this.setState({
+                     data: dataALL
+                })
 
-                };
+            })
 
-                return res;
-            });
-        this.setState({data});
+
     }
 
     render() {
-        const {data} = this.state;
+        let {data} = this.state;
 
         if (!this.state.data.length) {
             return 'Loading...';
         }
-
 
         return (
             <div>
