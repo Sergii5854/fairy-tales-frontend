4df9a91b643fd49e94d5a3814c5de788caa61d06
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-26 15:10:08 +0300

    add routers functionality folk and author tales

--- src/routes/folk/index.js
+++ src/routes/folk/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import Header from './../../components/header';
+import React, {Component} from 'react';
+import DataTable from '../../components/data-table';
+import Header from './../../components/header/';
+import axios from 'axios';
 
-const Folk = () => {
-    return(
-        <div>
-            <Header
-                title="Казки дитинства"
-                subTitle="Народні"
-                background="#dc23276b"
-            />
-        </div>
+export default class Folk extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
 
-    )
+    componentDidMount() {
+        axios.get('https://tales-server.herokuapp.com/api/v1/folk')
+            .then(response => {
+                let data = response.data['folkFairyTales']
+
+
+                let dataALL = data
+                    .map(({id, name,}) => {
+                        const res = {
+                            '#': Number(id),
+                            'Name': name
+                        };
+
+                        return res;
+                    });
+                this.setState({
+                    data: dataALL
+                })
+            })
+    }
+
+    render() {
+        let {data} = this.state;
+
+        if (!this.state.data.length) {
+            return 'Loading...';
+        }
+        return (
+          <div>
+              <Header
+                  title="Казки дитинства"
+                  subTitle="Народні"
+                  background="#dc23276b"
+              />
+              <DataTable data={data}/>
+          </div>
+        )
+    }
 };
-
-export default Folk;
