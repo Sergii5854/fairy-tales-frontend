b307bacb514ce3f3e8dc995e58a8508b3ca52108
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-03-27 21:54:26 +0300

    fix functional item- fairytales

--- src/components/item-fairytales/index.js
+++ src/components/item-fairytales/index.js
@@ -1,4 +1,4 @@
-import React,  {Component} from 'react';
+import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 const ItemFaIrytailes = ({audioUrl, id, imageUrl, lullaby, name, text, updateTime, updated}) => {
--- src/routes/item-fairytele/index.js
+++ src/routes/item-fairytele/index.js
@@ -4,16 +4,44 @@ import ItemFairytele from './../../components/item-fairytales/'
 import axios from 'axios'
 
 class itemFairytel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        }
+    }
     componentDidMount () {
     console.log(this.props.match.params.id);
       axios.get(`https://tales-server.herokuapp.com/api/v1/fairytales/${this.props.match.params.id}`)
           .then(response => {
               let data = response.data['fairytales']
-              console.log(data);
+              let dataALL = data
+                  .map(({id, name,text,audioUrl}) => {
+                      const res = {
+                          'id': Number(id),
+                          'name': name,
+                          'text': text,
+                          'audioUrl':audioUrl
+                      };
+
+                      return res;
+                  });
+              this.setState({
+                  data: dataALL
+              })
+
           })
     }
     render(){
-    return(
+
+        let {data} = this.state;
+
+        if (!this.state.data.length) {
+            return 'Loading...';
+        }
+        console.log(this.state.data, "state2");
+
+        return(
         <div>
             <Header
                 title="Казки дитинства"
@@ -21,6 +49,11 @@ class itemFairytel extends Component {
                 background="#ee76276b"
             />
             <ItemFairytele
+
+                name={this.state.data[0].name}
+                text={this.state.data[0].text}
+                audioUrl={this.state.data[0].audioUrl}
+                id={this.state.data[0].id}
             />
         </div>
     )
