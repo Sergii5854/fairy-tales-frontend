6eb61d5f8519873eeb9372a38604cd5dee94dc35
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-04-06 10:29:21 +0300

    add favicon v5

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+let FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const webpack = require('webpack');
 
 module.exports = {
@@ -19,6 +20,39 @@ module.exports = {
             'process.env': {
                 API_KEY: JSON.stringify(process.env.API_KEY),
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new FaviconsWebpackPlugin({
+            // Your source logo
+            logo: './favicon/icon.png',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icons-[hash]/',
+            // Emit all stats of the generated icons
+            emitStats: false,
+            // The name of the json containing all favicon information
+            statsFilename: 'iconstats-[hash].json',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+            title: 'Webpack App',
+
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
             }
         })
     ],
